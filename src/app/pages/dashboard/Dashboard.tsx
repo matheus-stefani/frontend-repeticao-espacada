b1b430833f83b68b45bc298b466ad18ceb300a04
MatheusStefaniDosSantos <matheusstefani8@gmail.com>
2023-10-11 18:14:44 -0300

    feat: lista

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,10 +1,28 @@
+import { useState } from "react";
 
+export const Dashboard = () => {
+  const [lista, setLista] = useState<string[]>([]);
+  const [nome, setNome] = useState<string>("");
 
-export const Dashboard = ()=>{
-
-    return (
-        <>
-        <h1>Camo</h1>
-        </>
-    )
-}
+  const handleClick = () => {
+    setLista([...lista, nome]);
+    setNome("");
+  };
+  return (
+    <div>
+      <label>Insira um nome:</label>
+      <input
+        type="text"
+        value={nome}
+        onChange={(e) => setNome(e.target.value)}
+      />
+      <br />
+      <button onClick={handleClick}>Clique</button>
+      <ul>
+        {lista.map((e) => {
+          return <li>{e}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
